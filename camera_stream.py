40a6a503eb309d3755143bca0a4e667e6ac2abfe
Blackbear000 <zlin47@ucsc.edu>
2023-11-03 02:32:35 +0800

    detect

--- camera_stream.py
+++ camera_stream.py
@@ -150,8 +150,8 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/process', methods=['POST'])
-def process():
+@app.route('/detect', methods=['POST'])
+def detect():
 
     picture = request.files.get('picture')
     download_path = '/poseDetect/'
@@ -203,10 +203,68 @@ def process():
             print('Predicted Class:', predicted_class.item())
 
             # 返回处理后的图像路径
-            return jsonify({'skeleton_image_path': 'static/skeleton.jpg'})
+            return jsonify({'skeleton_image_path': 'static/skeleton.jpg', 'predicted_class': predicted_class.item()})
     except Exception as e:
         app.logger.error(f"Error processing image: {str(e)}")
         return jsonify({'error': 'An error occurred while processing the image'}), 500
+
+@app.route('/process', methods=['POST'])
+def process():
+
+    picture = request.files.get('picture')
+    # download_path = '/poseDetect/'
+    # torch.hub.set_dir(download_path)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # # model = CustomDeepModel(num_classes=3)
+    # model = ResNet18(num_classes=3).to(device)
+    # model.load_state_dict(torch.load('poseDetect/model_Resnet18_best.pth', map_location=torch.device('cpu')))
+    # model=model.to(device)
+    # model.eval()
+
+    # try:
+    #     # 创建一个临时文件来保存上传的图像
+    #     with tempfile.NamedTemporaryFile(delete=False, suffix='.jpg') as temp_file:
+    #         temp_file_path = temp_file.name
+    #         picture.save(temp_file_path)
+
+    #         # 读取上传的图像
+    #         image = cv2.imread(temp_file_path)
+
+    #         # 在这里进行图像处理
+    #         skeleton_image = process_image(image)
+            
+    #         # 保存骨架图像到静态目录
+    #         cv2.imwrite('static/skeleton.jpg', skeleton_image)
+
+    #         new_image_path = 'static/skeleton.jpg'
+
+    #         # 预处理图像
+    #         input_image = preprocess_image(new_image_path)
+
+    #         # 将输入图像移到GPU上（如果可用的话）
+    #         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #         input_image = input_image.to(device)
+    #         # print('input_image Shape:', input_image.shape)
+
+    #         # 使用模型进行预测
+    #         with torch.no_grad():
+    #             outputs = model(input_image)
+
+    #         # 获取预测结果
+    #         print('Model Output Shape:', outputs.shape)
+    #         _, predicted_class = torch.max(outputs, 1)
+    #         # 打印预测的类别
+
+    #         # print('Predicted Class:', predicted_class.tolist())
+
+    #         print('Predicted Class:', predicted_class.item())
+
+    #         # 返回处理后的图像路径
+    #         return jsonify({'skeleton_image_path': 'static/skeleton.jpg'})
+    # except Exception as e:
+    #     app.logger.error(f"Error processing image: {str(e)}")
+    #     return jsonify({'error': 'An error occurred while processing the image'}), 500
 
 
     # detect_main(picture)
